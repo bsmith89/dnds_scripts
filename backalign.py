bb9b34b9caa193df2c3f0e748811bd4532100643
Byron Smith <bsmith89@gmail.com>
2012-03-04 23:16:54 -0500

    First commit. Working version.

---
+++ backalign.py
@@ -0,0 +1,89 @@
+"""Correctly aligns nucleotide sequences (codon-wise).
+
+Takes AA-alignments with their associated nucleotide sequences and
+outputs the aligned nucleotides.
+
+TODO: Figure out how to deal with AA seqs that aren't the correct
+length.
+"""
+import optparse
+import sys
+import Bio
+from Bio import SeqIO, Alphabet, Seq
+
+def get_seq_record(ident, seq_recs):
+    """Returns the first seq record with the provided name."""
+    for seq_rec in seq_recs:
+        if seq_rec.id == ident:
+            return seq_rec
+
+def back_align_all(nucl_recs, aa_recs):
+    out_nucl_recs = []
+    for nucl_rec in nucl_recs:
+        aa_rec = get_seq_record(ident = nucl_rec.id, seq_recs = aa_recs)
+        try:
+            nucl_rec.seq = back_align(nucl_rec, aa_rec)
+        except ValueError:
+            sys.stderr.write("WARNING: nucl_seq '%s' and aa_seq '%s' are not of matching lengths!\n%d and %d positions long respectively (w/out gaps)\nSequences removed from analysis.\n" % (nucl_rec.id, aa_rec.id, len(nucl_rec.seq.ungap(gap = '-')), len(aa_rec.seq.ungap(gap = '-'))))
+            nucl_rec.seq = Bio.Seq.Seq('-', Bio.Alphabet.generic_dna)
+        else:
+            out_nucl_recs += [nucl_rec]
+    return out_nucl_recs
+
+def codons(nucl_seq):
+    """Produces a codon iterator from a nucleotide sequence."""
+    assert len(nucl_seq) % 3 == 0
+    for i in range(len(nucl_seq) / 3):
+        yield nucl_seq[i * 3:(i * 3) + 3]
+
+def back_align(nucl_rec, aa_rec):
+    """Takes a aligned AAs and matches nucleotides to them."""
+    nucl_seq = nucl_rec.seq.ungap(gap = '-')
+    aa_seq = aa_rec.seq
+    out_seq = Bio.Seq.Seq("", alphabet = Bio.Alphabet.generic_dna)
+    if not (len(nucl_seq) - 3) / 3.0 == len(aa_seq.ungap(gap = '-')):
+        raise ValueError('Sequence lengths do not match.')
+    codon_gen = codons(nucl_seq)
+    for aa in aa_seq:
+        if aa is not "-":
+            out_seq += codon_gen.next()
+        else:
+            out_seq += "---"
+    assert len(out_seq) / 3.0 == len(aa_seq)
+    return out_seq
+
+def main():
+    usage = "usage: %prog [options] [nucl] [aligned-aa]"
+    parser = optparse.OptionParser(usage = usage)
+    parser.add_option("-n", "--nucl", "--in", dest = "nucl_file",
+                      help = "unaligned nucleotides in FASTA format")
+    parser.add_option("-a", "--aa", "--alignment", dest = "aa_file",
+                      help = "aligned amino acids in FASTA format")
+    parser.add_option("-o", "--out", dest = "out_file",
+                      help = "filename for aligned nucleotides output")
+    (opts, args) = parser.parse_args()
+    nucl_file = None
+    aa_file = None
+    out_file = None
+    if opts.nucl_file is not None:
+        nucl_file = open(opts.nucl_file)
+    elif len(args) >= 1:
+        nucl_file = open(args[0])
+    else:
+        nucl_file = sys.stdin
+    if opts.aa_file is not None:
+        aa_file = open(opts.aa_file)
+    elif len(args) >= 2:
+        aa_file = open(args[1])
+    else:
+        raise TypeError("No aa_file argument or option given")
+    if opts.out_file is not None:
+        out_file = open(opts.out_file, 'w')
+    else:
+        out_file = sys.stdout
+    nucl_recs = list(Bio.SeqIO.parse(nucl_file, 'fasta', alphabet = Bio.Alphabet.generic_dna))
+    aa_recs = list(Bio.SeqIO.parse(aa_file, 'fasta', alphabet = Bio.Alphabet.generic_protein))
+    Bio.SeqIO.write(back_align_all(nucl_recs, aa_recs), out_file, 'fasta')
+
+if __name__ == "__main__":
+    main()
