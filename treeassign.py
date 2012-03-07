fa534fead2fc1003b2aebd1909810955c5c1ef77
Byron Smith <bsmith89@gmail.com>
2012-03-07 15:55:03 -0500

    Now it works with unlabeled leaves

--- treeassign.py
+++ treeassign.py
@@ -13,23 +13,19 @@ def recurse_label(clade, target, design, labels):
     Just pseudo-code right now
     """
     if clade.is_terminal(): # if it's a leaf
-        sys.stderr.write("Found leaf %s\n" % clade.name)
         try:
             if design[clade.name] is target:
-                label_leaf(clade, labels[target])
                 return True
             else:
                 return False
-        # if the node is not in the design mapping, then it's
-        # either been excluded or it's already been labeled.
-        # Either way it isn't the target grouping.
+        # If the node is not in the design mapping ignore it
+        # and don't consider it in marking higher level nodes.
         except KeyError:
-            return False
+            return None # or should it be True? nothing at all?
     else: # if it's a node
-        sys.stderr.write("Entered node %s\n" % repr(clade))
         all_in_group = True
         for subclade in clade.clades:
-            if not recurse_label(subclade, target, design, labels):
+            if recurse_label(subclade, target, design, labels) is False:
                 all_in_group = False
         if all_in_group is False:
             return False
@@ -38,10 +34,10 @@ def recurse_label(clade, target, design, labels):
             return True
 
 def label_leaf(leaf, label):
-    leaf.name += label
+    leaf.name += " %s" % label
 
 def label_node(node, label):
-    node.confidence = label
+    node.confidence = " %s" % label
 
 def main():
     design_path = sys.argv[1]
@@ -59,17 +55,20 @@ def main():
         design[item] = grouping
         if labels_file is None and grouping not in labels:
             labels[grouping] = "#%d" % (len(labels) + 1)
-    sys.stderr.write("%s\n" % str(design))
     if labels_file is not None:
         for mapping in labels_file:
             grouping, label = mapping.split()
             labels[grouping] = label
-    sys.stderr.write("%s\n" % str(labels))
     # Now actually do the labeling...
     for grouping in labels:
-        sys.stderr.write("Applying labels to group %s\n" % grouping)
         recurse_label(tree.clade, grouping, design, labels)
-    sys.stderr.write("%s\n" % str(tree))
+    # Because labeling directly changes the name, I can't label
+    # leaves before I'm done with all of the nodes.
+    for leaf in tree.get_terminals():
+        try:
+            label_leaf(leaf, labels[design[leaf.name]])
+        except KeyError:
+            continue
     Bio.Phylo.write(tree, sys.stdout, 'newick', format_confidence = '%s')
 
 if __name__ == '__main__':
