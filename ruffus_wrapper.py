3d8b1ad8efca5c3da541a747b4fb614a24edc307
Byron Smith <bsmith89@gmail.com>
2012-04-02 18:24:36 -0400

    Blegh...

--- ruffus_wrapper.py
+++ ruffus_wrapper.py
@@ -17,6 +17,7 @@ Use like so:
 ...     "Make an input file."
 ...     def args_gen(self):
 ...         yield([], [self.pipeline_metadata.get_path('input')])
+...
 ...     def __call__(self, input_path_list, output_path_list):
 ...         with open(output_path_list[0], 'w') as output_file:
 ...             output_file.write("This is\nAn Output\n file.")
@@ -26,13 +27,14 @@ Use like so:
 ...     def args_gen(self):
 ...         yield([self.pipeline_metadata.get_path('input')],
 ...               [self.pipeline_metadata.get_path('output')])
+...
 ...     def __call__(self, input_path_list, output_path_list):
-...         with open(input_path_list[0]) as input_file, open(output_path_list[0], 'w') as output_file:
+...         with open(input_path_list[0]) as input_file, \
+...              open(output_path_list[0], 'w') as output_file:
 ...             for line in input_file:
 ...                 output_file.write(line)
 ...
 ... class Foo_pipeline(Pipeline):
-...     _task_classes = set([Foo_task1, Foo_task2])
 ...     _topology = dict(Foo_task1 = [], Foo_task2 = [Foo_task1])
 ...
 ... md = Foo_md()
@@ -40,8 +42,10 @@ Use like so:
 ... f.run()
 
 """
+import re
 import ruffus
 from ruffus.graph import node as ruffus_nodes
+
 
 class PipelineMetadata(object):
     _path_tmplts = {}
@@ -64,82 +68,143 @@ class Task(object):
     def args_gen(self, *args, **kwargs):
         raise NotImplementedError("You must define args_gen for each Task subclass.")
     
-    def up2date_checker(self, inputs_list, outputs_list, *args, **kwargs):
-        raise NotImplementedError("You must define up2date_checker for each Task subclass.\n\
-If it is not defined the default ruffus up-to-date checker will run.")
-        # Consider removing this from the base_class, since I don't think
-        # we want to make pipeline think that it has an up2date_checker
-        # method when it really doesn't.
+#    def up2date_checker(self, inputs_list, outputs_list, *args, **kwargs):
+#        raise NotImplementedError("You must define up2date_checker for each Task subclass.")
+#        # we don't want to make pipeline think that it has an up2date_checker
+#        # method when it really doesn't.
     
     def __call__(self, inputs_list, outputs_list, *args, **kwargs):
         raise NotImplementedError("You must define __call__ for each Task subclass.")
 
 class Pipeline(object):
-    _task_classes = set()
     _topology = {}
     
     def __init__(self, pipeline_metadata):
         self.pipeline_metadata = pipeline_metadata
-        self._initialized = {}
-        self._tasks = []
-        for task_class in self._task_classes:
-            self._tasks += [task_class(self.pipeline_metadata)]
-        for task in self._tasks:
-            self.initialize(task)
+        # task is always the task-class
+        # use self.function() to get the name of the
+        # wrapped ruffus function.
+        for task in self._tasks():
+            self._initialize(task(self.pipeline_metadata))
+                
+    def _function(self, task):
+        """Return the name of the global function which executes the task."""
+        return "%s_%s" % (self.__class__.__name__.lower(), task.__name__.lower())
         
-    def get_follows(self, task):
-        follows_list = self._topology.get(task.__class__)
-        return follows_list
+    def _get_follows(self, task):
+        """Return the names of all functions which task follows."""
+        task_list = self._topology.get(task.__class__)
+        function_list = []
+        for task in task_list:
+            function_list += [self._function(task)]
+        return function_list
     
-    def tasks(self):
-        for task in self._tasks:
-            yield task
-            
-    def initialized(self, task):
-        if self._initialized.get(task) is True:
-            return True
+    def _tasks(self):
+        for key in self._topology:
+            yield key
+        
+    def _initialize(self, task_object):
+        print("Init'ing a task.")
+        
+        if hasattr(task_object, 'up2date_checker'):
+            @ruffus.follows(*self._get_follows(task_object))
+            @ruffus.files(task_object.args_gen)
+            @ruffus.check_if_uptodate(task_object.up2date_checker)
+            def wrapped_task(*args, **kwargs):
+                task_object(*args, **kwargs)
         else:
-            return False
-        
-    def initialize(self, task):
-        if not self.initialized(task):
-            if hasattr(task, 'up2date_checker'):
-                @ruffus.follows(*self.get_follows(task))
-                @ruffus.files(task.args_gen)
-                @ruffus.check_if_uptodate(task.up2date_checker)
-                def wrapped_task(*args, **kwargs):
-                    task(*args, **kwargs)
-            else:
-                @ruffus.follows(*self.get_follows(task))
-                @ruffus.files(task.args_gen)
-                def wrapped_task(*args, **kwargs):
-                    task(*args, **kwargs)
-            # It looks like I should be hacking a bunch of other
-            # attributes in ruffus_nodes. :(
-            # TODO:
-            method_name = "_%s" % task.__class__.__name__.lower()
-            self.__setattr__(method_name, wrapped_task)
-            # This whole block here is a hack to change the ruffus-saved name of
-            # the task to what I want, which is the Task class-name.
-            dummy_node_name = '%s.wrapped_task' % wrapped_task.__module__
-            new_node_name = '%s.%s' % (__name__, task.__class__.__name__)
-            # is it possible that by using self.__class__ instead of
-            # wrapped_task.__module__, that I could define multiple pipelines
-            # in one module?  This would be optimal! 
-            ruffus_node = ruffus_nodes.lookup_node_from_name(dummy_node_name)
-            ruffus_nodes._name_to_node[new_node_name] = ruffus_node
-            ruffus_node.semaphore_name = new_node_name
-            del ruffus_nodes._name_to_node[dummy_node_name]
-            # So will this task (node in ruffus speak) be 'findable' by other tasks with
-            # incoming or outgoing dependencies?
-            self._initialized[task] = True
-            
-    def all_initialized(self):
-        for task in self.tasks():
-            if not self.initialized(task):
-                return False
-        return True
-            
-    def run(self, *args, **kwargs):
-        ruffus.pipeline_run([self.tasks[-1]])
+            @ruffus.follows(*self._get_follows(task_object))
+            @ruffus.files(task_object.args_gen)
+            def wrapped_task(*args, **kwargs):
+                task_object(*args, **kwargs)
+        # Now I have a wrapped function, but it's only defined in the local
+        # name space, and all of the assignments in ruffus.graph.node are
+        # incorrect.  I need to reassign basically everything in
+        # the _task function to point to a new global (i.e. module level)
+        # function which was assigned to this wrapped function
+        function_name = self._function(task_object.__class__)
+        # set the correct global reference to function
+        globals()[function_name] = wrapped_task
+        wrapped_task.__name__ = function_name
+        wrapped_task.__doc__ = task_object.__call__.__doc__
+        task_name = "%s.%s" % (__name__, function_name)
+        dummy_name = "%s.%s" % (__name__, 'wrapped_task')
+        ruffus_task = ruffus_nodes._name_to_node[dummy_name]
+        if task_object.__doc__:
+            # dunno what this does, but it's how ruffus writes the _description
+            ruffus_task._description = re.sub("\n\s+", " ",
+                                              task_object.__doc__).strip()
+        else:
+            ruffus_task._description = ""
+        ruffus_task._func_name = function_name
+        ruffus_task._name = task_name
+        ruffus_task.semaphore_name = task_name
+        ruffus_task.user_defined_work_func = globals()[function_name]
+        if task_name in ruffus_nodes._name_to_node:
+            # then the task object was already created with the intention
+            # of assigning most of its properties in the future.
+            # The only important property seems to be _inward which 
+            # had a task object added to it every time
+            # the function name was found in a follows statement.
+            ruffus_task._outward = ruffus_nodes._name_to_node[task_name]._outward
+            ruffus_task._inward = ruffus_nodes._name_to_node[task_name]._inward
+            ruffus_nodes._all_nodes.remove(ruffus_nodes._name_to_node[task_name])
+        ruffus_nodes._name_to_node[task_name] = ruffus_task
+        # remove the originally assigned, incorrect _task object from two places:
+        del ruffus_nodes._name_to_node[dummy_name]
+                    
+    def __call__(self, *args, **kwargs):
+        ruffus.pipeline_run(*args, **kwargs)
     
+def reset_ruffus():
+    ruffus_nodes._all_nodes = []
+    ruffus_nodes._name_to_node = {}
+    ruffus_nodes._global_node_index = 0
+        
+#if __name__ == "__main__":
+if __name__ == "__main__":
+    class Foo_md(PipelineMetadata):
+        _path_tmplts = dict(input = 'test.in', output = 'test.out')
+    
+    class Foo_task1(Task):
+        def args_gen(self):
+            yield([], [self.pipeline_metadata.get_path('input')])
+        
+        def __call__(self, inputs_list, outputs_list):
+            with open(outputs_list[0], 'w') as outfile:
+                outfile.write("This is\nAn output\n f.")
+    
+    class Foo_task2(Task):
+        def args_gen(self):
+            yield ([self.pipeline_metadata.get_path('input')],
+                   [self.pipeline_metadata.get_path('output')])
+        
+        def __call__(self, inputs_list, outputs_list):
+            with open(inputs_list[0]) as infile, open(inputs_list[0], 'w') as outfile:
+                for line in infile:
+                    outfile.write(line)
+                    
+    class Foo_pipeline(Pipeline):
+        _topology = {Foo_task1:[], Foo_task2:[Foo_task1]}
+    
+    f = Foo_pipeline(Foo_md())
+    #f()
+    reset_ruffus()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
