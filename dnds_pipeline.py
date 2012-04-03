e7ce2614310a060a43eaba25e30f9279b0ab235f
Byron Smith <bsmith89@gmail.com>
2012-04-03 14:05:30 -0400

    ...

--- dnds_pipeline.py
+++ dnds_pipeline.py
@@ -8,7 +8,7 @@ import functools
 class Metadata(PipelineMetadata):
     _path_tmplts = dict(input = 'test.in', output = 'test.out')
 
-class task1(Task):
+class Task1(Task):
     "Make an input file."
     def args_gen(self):
         yield([], [self.pipeline_metadata.get_path('input')])
@@ -21,16 +21,15 @@ class task1(Task):
 def get_params():
     pass
 
-
 if __name__ == "__main__":
     run_args, run_kwargs, metadata_args, metadata_kwargs = get_params()
     metadata = Metadata(*metadata_args, **metadata_kwargs)
 
-    _task2 = Foo_task2(md)
-    @ruffus.follows(task1)
-    @ruffus.files(_task2.args_gen)
-    @functools.wraps(Foo_task2, assigned = ['__doc__'])
-    def task2(*args, **kwargs):
-        _task2(*args, **kwargs)
-
-    ruffus.pipeline_run([task2], verbose = 5, multiprocess = 2)
+    _task1 = Task1(metadata)
+    @ruffus.follows()
+    @ruffus.files(_task1.args_gen)
+    @functools.wraps(Task1, assigned = ['__doc__'])
+    def task1(*args, **kwargs):
+        _task1(*args, **kwargs)
+        
+    ruffus.pipeline_run()
