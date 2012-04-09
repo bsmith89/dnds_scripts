8a9565a710164540da68a5de38c11b93d3df0506
Byron Smith <bsmith89@gmail.com>
2012-04-09 18:53:18 -0400

    Gahhh

--- pipeline.py
+++ pipeline.py
@@ -67,7 +67,8 @@ For example, a pipeline module might look like this:
 
 TODO: Update the example.  Won't work currently.
 """
-
+import ruffus
+import functools
 
 class BaseMetadata(object):
     _path_tmplts = {}
@@ -98,3 +99,85 @@ class BaseTask(object):
     
     def __call__(self, inputs_list, outputs_list, *args, **kwargs):
         raise NotImplementedError("You must define __call__ for each Task subclass.")
+    
+class BasePipeline(object):
+    _topology = {}
+    
+    def __init__(self, pipeline_metadata):
+        self.pipeline_metadata = pipeline_metadata
+        for task_class in self._topology:
+            self._initialize(task_class)
+            
+    def _function(self, task_class):
+        class_string = self.__class__.__name__.lower()
+        task_string = task_class.__name__.lower()
+        function_string = "{class_string}__{task_string}".format(class_string = \
+                                                                 class_string, 
+                                                                 task_string = \
+                                                                 task_string)
+        return function_string
+    
+    def _dependencies(self, task_class):
+        dep_classes = self._topology[task_class]
+        dep_functions = []
+        for dep_class in dep_classes:
+            dep_functions += [self._function(dep_class)]
+        return dep_functions
+    
+    def _initialize(self, task_class):
+        if hasattr(task_class, 'up2date_checker'):
+            code = """
+@ruffus.follows(self._dependencies(task_class))
+@ruffus.files(task_object.args_gen)
+@ruffus.check_if_uptodate(task_object.up2date_checker)
+@functools.wraps(task_class, __doc__)
+def {function}(*args, **kwargs):
+    return task_object(*args, **kwargs)
+"""
+        else:
+            code = """
+@ruffus.follows(self._dependencies(task_class))
+@ruffus.files(task_object.args_gen)
+@functools.wraps(task_class, '__doc__')
+def {function}(*args, **kwargs):
+    return task_object(*args, **kwargs)
+"""
+        code = code.format(function = self._function(task_class))
+        print(code)
+        exec(code, globals(), dict(self = self,
+                                   task_class = task_class,
+                                   task_object = task_class(self.pipeline_metadata)))
+    
+    def __call__(self):
+        pass
+        
+        
+        
+        
+        
+        
+def test_exec(function_name):
+    class FooTask(BaseTask):
+        """Docstring"""
+        pass
+    def d(task_class):
+        return ['some', 'depends']
+    self = BasePipeline('md')
+    self._dependencies = d
+    task_class = FooTask
+    task_object = FooTask(self.pipeline_metadata)
+    code = """
+@ruffus.follows(*self._dependencies(task_class))
+@ruffus.files(task_object.args_gen)
+@ruffus.check_if_uptodate(task_object.up2date_checker)
+@functools.wraps(task_class, ['__doc__'])
+def {function}(*args, **kwargs):
+    return task_object(*args, **kwargs)
+"""
+    code = code.format(function = function_name)
+    exec(code, globals(), dict(ruffus = ruffus,
+                               self = self,
+                               task_class = task_class,
+                               task_object = task_object))
+        
+        
